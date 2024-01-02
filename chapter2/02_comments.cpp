72f22a96f81eb1c33e5bbbc4f4e5cdd1da76045d
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-01-02 11:34:54 +0530

    Create 02_comments.cpp

---
+++ chapter2/02_comments.cpp
@@ -0,0 +1,12 @@
+// This is a way of comment a line of code.
+
+/* This is a second way to comment any line or paragraph at a same time */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}

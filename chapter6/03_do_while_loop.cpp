e2c7c050bb98c5f3b310623fd6792eb89f2cc2e5
Ayush Singh <115815007+ayushsingh119@users.noreply.github.com>
2024-01-06 20:14:05 +0530

    Create 03_do_while_loop.cpp

---
+++ chapter6/03_do_while_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    do
+    {
+        cout << i << " I love c++" << endl;
+        i++;
+    } while (i < 10);
+    return 0;
+}
